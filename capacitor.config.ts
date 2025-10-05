ed04f3ab80c14ef704779bdb5bdc10e93ef24480
josrompoz1 <josrompoz1@gmail.com>
2025-10-05 13:10:10 +0200

    Migrating to Ionic 8 project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sabrosona-food',
+  webDir: 'www'
+};
+
+export default config;
